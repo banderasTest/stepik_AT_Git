428056895e9c115d7bdcc4be187a1b9dae2edac6
Poddubniy <poddubniy@magnit.ru>
2024-05-16 11:28:22 +0300

    ожидаемые состояния

--- Expected conditions.py
+++ Expected conditions.py
@@ -3,31 +3,31 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import math
-def calc(x):
- return str(math.log(abs(12*math.sin(int(x)))))
 
-browser = webdriver.Chrome()
-
-browser.get("http://suninjuly.github.io/explicit_wait2.html")
-
-# говорим Selenium проверять в течение 15 секунд, пока цена объекта не станет равной $100
-price = WebDriverWait(browser, 15).until(
+    def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+	 
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+    browser = webdriver.Chrome()
+    
+    # говорим Selenium проверять в течение 15 секунд, пока цена объекта не станет равной $100
+    price = WebDriverWait(browser, 15).until(
         EC.text_to_be_present_in_element((By.ID, "price"),"$100")
     )
-button = browser.find_element(By.ID, "book").click()
+    button = browser.find_element(By.ID, "book").click()
 
-# находим элемент, содержащий текст
-x_element = browser.find_element(By.ID, 'input_value')
-x = x_element.text
-y = calc(x)
+    # находим элемент, содержащий текст
+    x_element = browser.find_element(By.ID, 'input_value')
+    x = x_element.text
+    y = calc(x)
 
-input1 = browser.find_element(By.ID, 'answer')
-input1.send_keys(y)
+    input1 = browser.find_element(By.ID, 'answer')
+    input1.send_keys(y)
 
-# говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной 2
-button = WebDriverWait(browser, 5).until(
+    # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной 4
+    button = WebDriverWait(browser, 5).until(
         EC.element_to_be_clickable((By.ID, "solve"))
     )
-button.click()
+    button.click()
 
-print(browser.switch_to.alert.text.split(': ')[-1])
+    print(browser.switch_to.alert.text.split(': ')[-1])
